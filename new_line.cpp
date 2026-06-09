2455438a4aabb17abbaaf00566da60f274de6df1
Deven Bhasin <d.bhasin@uqconnect.edu.au>
2026-06-10 00:27:14 +1000

    Learned new line

---
+++ new_line.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Shanker Bhasin" << "\n" << 50;
+    cout << "Aryan Bhasin" << endl << 17;
+    cout << "Deven Bhasin\n" << 22;
+
+    return 0;
+}
