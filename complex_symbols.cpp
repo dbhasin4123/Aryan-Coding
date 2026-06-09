9ac8af491a5c804f7a51db33ed5cf8f27feea392
Deven Bhasin <d.bhasin@uqconnect.edu.au>
2026-06-10 00:39:17 +1000

    learned syntax

---
+++ complex_symbols.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "\"Aryan bhasin is a good boy\"" << endl << "aryanbhasin8930@gmail.com" << endl << "Ritu Bhasin & Aryan Bhasin" << endl << "(This is a practice session)" << endl << "{Done by Ritu Bhasin}";
+    return 0;
+}
