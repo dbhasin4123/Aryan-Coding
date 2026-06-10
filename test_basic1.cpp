2db8a969791e79dc00353acc5610ee862c82c86c
Deven Bhasin <d.bhasin@uqconnect.edu.au>
2026-06-10 22:22:13 +1000

    Did my first test

---
+++ test_basic1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "I just finished my 12 standard";
+    cout << "\n";
+
+    cout << "Aryan Bhasin\t"<< 17 << endl;
+    
+    cout << "\"Loyalty is a two path process\"";
+
+    return 0;
+}
+
