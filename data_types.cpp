be4e545d594dc30acbfcfe425decccd5b8cf0534
Deven Bhasin <d.bhasin@uqconnect.edu.au>
2026-06-12 21:55:52 +1000

    learned datatypes

---
+++ data_types.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Q1 float and double precision comparison decimal value 15 
+    float a = 1.123456789101112;
+    double b = 1.123456789101112;
+    cout << "the value of a is: " << a << endl;
+    cout << "the value of b is: " << b << endl;
+
+    // a = 1.123456835746765
+    // b = 1.123456789101112
+
+    // B,S,n print ascii
+    char c = 66;
+    char d = 83;
+    char e = 110;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+
+    // auto datatype every datatype
+    auto n = 10;
+    auto f = 1.2f;
+    auto g = 1.123456789101112;
+    auto h = 'A';
+    auto i = "Hello World";
+    auto j = true;
+
+    cout << n << endl;
+    cout << f << endl;
+    cout << g << endl;
+    cout << h << endl;
+    cout << i << endl;
+    cout << j << endl;
+
+    return 0;
+}
+
+
